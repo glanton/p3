12fe36394ac05ca21949e62de0e28fa03c721439
Alex Friberg <classay@gmail.com>
2014-10-24 18:22:39 -0400

    post submission commit: fixed a couple of typos in the readme.md, made a minor adjustment so that EACH paragraph varies instead of setting a random number of sentences once and applying it to every paragraph, and added some comments while the code is still fresh in my mind

--- app/routes.php
+++ app/routes.php
@@ -54,6 +54,7 @@ Route::get('/text/{query}', function($query) {
 		$numberOfParagraphs = 50;
 	}
 	
+	// array of books to be ready--MUST match names of book files
 	$booksToRead = array("AnneOfGreenGables",
 				"Dracula",
 				"Frankenstein",
@@ -66,17 +67,21 @@ Route::get('/text/{query}', function($query) {
 				"TheScarletLetter");
             
         $openedBooks = array();
-            
+        
+	// iiterate through book files, creating strings and then turning each string into an array
 	$numberOfBooksToRead = count($booksToRead);
 	for ($i = 0; $i < $numberOfBooksToRead; $i++) {
 	    $bookString = file_get_contents(asset('books/' . $booksToRead[$i] . '.txt'));
 	    array_push($openedBooks, preg_split('/(?<!Mr\.)(?<!Mrs\.)(?<!Ms\.)(?<!Dr\.)(?<!St\.)(?<=[.?!])\s+(?=[a-z])/i', $bookString));
 	}
 	
-	$paragraphLength = rand(3, 6);
 	$generatedParagraphs = array();
 	
+	// create the indicated number of paragraphs with setences randomly drawn from the texts
 	for ($k = 0; $k < $numberOfParagraphs; $k++) {
+		// vary paragraph length with a limit so that things don't get out of control
+		$paragraphLength = rand(3, 6);
+		
 	    for ($i = 0; $i < $paragraphLength; $i++) {
 		$randomBook = rand(0, $numberOfBooksToRead - 1);
 		$randomSentence = rand(0, count($openedBooks[$randomBook]) - 1);
@@ -117,15 +122,19 @@ Route::get('/profile/{query}', function($query) {
 		$numberOfProfiles = 50;
 	}
 	
+	
+	// array of smiles used to generate profile pictures
 	$faces = array(': )', ':-)', ': ]', ':-]', ': }', ':-}',
                ': )', ':-(', ': [', ':-[', ': {', ':-{', ':, (',
                '; )', ';-)', '; ]', ';-]', '; }', ';-}',
                ': |', ':-|', ': /', ':-/', ': \\', ':-\\');
 	$facesLength = count($faces) - 1;
 	
+	// first check if profile picture was indicated, then generate
 	$generatedPictures = array();
 	if ($includePicture == true) {
 		for ($i = 0; $i < $numberOfProfiles; $i++) {
+		    // create image, set up colors, apply background colors, and then print random smiley
 		    $profilePic = imagecreate(200, 200);
 		    $background = imagecolorallocate($profilePic, 0, 0, 0);
 		    $text_color = imagecolorallocate($profilePic, 255, 255, 255);
@@ -139,6 +148,7 @@ Route::get('/profile/{query}', function($query) {
 		    imagefilledrectangle($profilePic, 0, 101, 100, 200, $sq_color4);
 		    imagettftext($profilePic, 130, -90, 55, 35, $text_color, public_path() . '/fonts/exprswy_free.ttf', $faces[rand(0, $facesLength)] );
 		    
+		    // encode image as base64
 		    ob_start();
 		    imagepng($profilePic);
 		    $profilePicData = ob_get_contents();
@@ -146,6 +156,7 @@ Route::get('/profile/{query}', function($query) {
 		    $profilePicDataBase64 = base64_encode ($profilePicData);
 		    $generatedPictures[$i] = $profilePicDataBase64;
 		    
+		    // free up image resources
 		    imagecolordeallocate($profilePic, $sq_color4);
 		    imagecolordeallocate($profilePic, $sq_color3);
 		    imagecolordeallocate($profilePic, $sq_color2);
@@ -156,14 +167,17 @@ Route::get('/profile/{query}', function($query) {
 		}
 	}
 	
+	// load first names
 	$firstNameString = file_get_contents(asset('names/first_names.txt'));
 	$firstNames = preg_split('/\s/', $firstNameString);
 	$numberOfFirstNames = count($firstNames);
 	
+	// load last names
 	$lastNameString = file_get_contents(asset('names/last_names.txt'));
 	$lastNames = preg_split('/\s/', $lastNameString);
 	$numberOfLastNames = count($lastNames);
 	
+	// generate randomly matched first and last names
 	$generatedNames = array();
 	for ($i = 0; $i < $numberOfProfiles; $i++) {
 		$randomFirstName = rand(0, $numberOfFirstNames - 1);
@@ -171,6 +185,7 @@ Route::get('/profile/{query}', function($query) {
 		$generatedNames[$i] = $firstNames[$randomFirstName] . ' ' . $lastNames[rand(0, $numberOfLastNames - 1)];
 	}
 	
+	// if indicated generate birthdays between January 1, 1901 and January 1, 2001
 	$generatedBirthdays = array();
 	if ($includeBirthday == true) {
 		$dateRangeLow = strtotime('1901-01-01');
@@ -182,6 +197,7 @@ Route::get('/profile/{query}', function($query) {
 		}
 	}
 	
+	// if indicated assign random locations
 	$generatedLocations = array();
 	if ($includeLocation == true) {
 		$locationString = file_get_contents(asset('locations/cities_countries.txt'));
@@ -194,6 +210,8 @@ Route::get('/profile/{query}', function($query) {
 		}
 	}
 	
+	
+	// if indicated load up books and pick a random sentence for each profile
 	$generatedQuotes = array();
 	if ($includeFavoriteQuote == true) {
 		$booksToRead = array("AnneOfGreenGables",
--- readme.md
+++ readme.md
@@ -10,13 +10,13 @@ Project Three of dwa15
 
 This is Project 3 (p3), also known as *Developer's Best Friend*. In my case we'll be calling it Litsum/proProfile, a one-two development punch.
 
-I chose ten well-known classics (which I've read, of course!) from available public domain books, and randomly mash their sentences together to create dummy paragraphs (what I'm calling Litsum).
+I chose ten well-known classics (which I've read, of course!) from available public domain books, and randomly mash their sentences together to create dummy paragraphs (what I'm calling Litsum), which works in similar fashion to various Lorem Ipsum generators strewn across the Internet.
 
-For the test profiles (the proProfile tool) I generate random images in php encoded as base64, and also have other normal options (birthdate, location). Using some Litsum code I provide the option to include a "favorite quote".
+For the test profiles (the proProfile tool) I generate random images in php encoded as base64, and also have a couple of standard options (birthdate, location). Using some Litsum code I provide the option to include a "favorite quote."
 
 ###Demo Info
 
-I will demo in-person.
+I will demo in-person next Thursday.
 
 ### Additional Info
 
@@ -24,17 +24,15 @@ There's a lot of credit to go around here, and I know I'll never be able to do j
 
 - Project Gutenberg, where I found complete text files for the ten books I used
 - The following authors and books: LM Montgomery - Anne of Green Gables, Brahm Stoker - Dracula, Mary Shelley - Frankenstein, Charles Dickens - Great Expectations, Joseph Conrad - Heart of Darkness, Charlotte Bronte - Jane Eyre, Herman Melville - Moby Dick, James Joyce - A Portrait of the Artist as a Young Man, Jane Austen - Pride and Prejudice, Nathaniel Hawthorne - The Scarlet Letter
-- thesitewizard, where I learned about generating images in php: http://www.thesitewizard.com/php/create-image.shtml
-- Ray Larabie, from Typodermic Fonts, who created and distributed for free the TTF file for Expressway, which turned out to be the perfect font for created the smileys on my generated profile pictures
-- CSS Sticky Footer, which didn't really make a different on my latop but should look nice on larger screens
+- [thesitewizard](http://www.thesitewizard.com/php/create-image.shtml), where I learned about generating images in php
+- Ray Larabie, from [Typodermic Fonts](http://typodermicfonts.com/), who created and distributed for free the TTF file for Expressway, which turned out to be the perfect font for created the smileys on my generated profile pictures
+- [CSS Sticky Footer](http://www.cssstickyfooter.com/), which didn't really make a difference on my latop but should look nice on larger screens
 - Wikipedia, where I found a nice city and country list
-- mongabay.com, which has a great list of the 1,000 most common last names in the United States
-- hadley/data-baby-names on Github, which gave me a huge and awesome list of first names
+- [mongabay.com](http://names.mongabay.com/data/1000.html), which has a great list of the 1,000 most common last names in the United States
+- [hadley](https://github.com/hadley/data-baby-names) on Github, which gave me a huge and awesome list of first names
 
 I'm happy with most of how the project came out, but the code is a lot less DRY than I would like. I think a lot of this could be fixed with a few good classes but I was already too far into the project when we started doing classes to want to switch. As it is routes.php is a little bit of a mess. The other major (and more noticeably) change I'd like to make is to make the book mash-up more efficient. Right now it loads the full content of all ten books to only pull out at most a few hundred sentences.
 
-I'd like to split the books into fairly uniform chunks, and add that as the top layer in randomly picking a sentence:
-
-pick book chunks -> pick sentences randomly from among those chunks
+I'd like to split the books into fairly uniform chunks, and pick a limited number of chunks from which to randomly draw the sentences of each paragraph. There cold potentially be a relationship of chunks per paragraph just to keep things diverse.
 
 I'd guess it would be possible to cut down more than 90% of the processing this way and still have a just-as-random mash-up. A more scalable solution would be to write a php script that checks the book folder for new books and then auto-indexes new ones for future use, but now I'm really getting ahead of myself!
